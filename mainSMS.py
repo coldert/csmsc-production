c09a7d8a30d603c5219f1a7225fb9e4ac160eeb9
Robert Wictorzon <robert@wictorzon.com>
2015-02-27 08:26:43 +0100

    All configuration in config.cfg

--- mainSMS.py
+++ mainSMS.py
@@ -7,13 +7,9 @@ import sms_ssh
 import sms_tolk
 import re
 
-# Read configuration files
+# Read configuration file
 conf = ConfigParser.ConfigParser()
 conf.read('config.cfg')
-users = ConfigParser.ConfigParser()
-users.read('users.cfg')
-hosts = ConfigParser.ConfigParser()
-hosts.read('hosts.cfg')
 
 # Function gets called from smsIO
 # by the handler when a GET request has been made. 
@@ -24,22 +20,24 @@ def incoming_sms(command_string, originator_string):
 	(command_host, command) = command_string.split('!')
 	# Parse command string into actual Cisco IOS commands
 	complete_cmd_string = sms_tolk.parse(command)
+	print complete_cmd_string #DEBUGGING
 	
 	# originator must be in format 0046XXXXXXXXXXX
 	originator = parse_phone(originator_string) if originator_string != "" else None
 	(username, password) = get_credentials(originator)
 	host_ip = get_host_ip(command_host)
+	print username, password, host_ip #DEBUGGING
 
 	# Send a command to a host via SSH and returns the output
 	try:
 		recv_host_output = sms_ssh.ssh_connect(host_ip, complete_cmd_string, username, password)
 	except Exception as e:
-		recv_host_output = "Something went wrong. " + str(e.errno) + ": " + e.strerror
+		recv_host_output = "Something went wrong."
 		
 	# Send a reply to the user via the sms gateway
 	if originator:
 		print "SENDING SMS..."
-		#send_sms(originator, recv_host_output, conf.get('cellsynt', 'user'), conf.get('cellsynt', 'pass'))
+		#send_sms(originator, recv_host_output, conf.get('smsgateway', 'user'), conf.get('smsgateway', 'pass'))
 	else:
 		print "NO ORIGINATOR"
 	print recv_host_output
@@ -51,12 +49,12 @@ def parse_phone(phone_number):
 	
 # Get username/password from list of phonenumbers
 def get_credentials(phone_number):
-	user = users.get('users', phone_number)
+	user = config.get('users', phone_number)
 	return user.split(':') if user else []
 
 # Get host IP from list of hostnames
 def get_host_ip(hostname):
-	host = hosts.get('hosts', hostname)
+	host = config.get('hosts', hostname)
 	return host if host else None
 
 # Function for sending SMS data back to the SMS gateway for handling
@@ -69,10 +67,10 @@ def send_sms(recv, msg, user, passw):
 	text = 'text=' + quote(msg)
 	
 	# Makes the PATH for the GET message
-	gateway_request = "/" + conf.get('cellsynt', 'gateway_file') + username + '&' + password + '&type=text&' + dest + '&charset=' + conf.get('cellsynt', 'charset') + '&' + text
+	gateway_request = "/" + conf.get('smsgateway', 'gateway_file') + username + '&' + password + '&type=text&' + dest + '&charset=' + conf.get('smsgateway', 'charset') + '&' + text
 	print gateway_request
 	# Working HTTP GET, 'gateway_url' is the url from which you want to use GET on. NOTE: Tested against 'https://docs.python.org/2/'
-	http_connection = httplib.HTTPConnection(conf.get('cellsynt', 'gateway_url'))
+	http_connection = httplib.HTTPConnection(conf.get('smsgateway', 'gateway_url'))
 	# Calling a function of the http connection object, from which we use GET with the specified path('gateway_request')
 	http_connection.request('GET', gateway_request)
 
